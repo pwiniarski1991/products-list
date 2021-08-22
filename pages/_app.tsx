e14c13dd787d5c0efec402b0b7ccf87ac115ef3e
Piotr <piotr.winiarski@300brains.com>
2021-08-22 22:31:13 +0200

    feat: update dependencies, use ModalContext and update MainComponent

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { ModalContext, ModalContextProvider } from './../contexts/modalContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ModalContextProvider>
+      <Component {...pageProps} />
+      <ModalContext.Consumer>
+        {({ setModalRef }): JSX.Element => <div ref={setModalRef} />}
+      </ModalContext.Consumer>
+    </ModalContextProvider>
+  )
 }
 
 export default MyApp
